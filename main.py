33801f812f65596515683421fd062e643c82bab1
Carlos Ayala <carlos.ayala@contractor.hallmark.com>
2021-11-30 21:55:05 -0500

    Punto4

--- main.py
+++ main.py
@@ -1,102 +1,31 @@
-#Ref dataset = https://drive.google.com/u/0/uc?id=15yR9VskbAsWoLerZuwf3_XrxbmC0_ykG&export=download
-#Author = Carlos Ayala
 
 import pandas as pd
+import matplotlib.pyplot as plt
+
 url = 'covid_22_noviembre.csv'
-df = pd.read_csv(url)
+df = pd.read_csv(url, low_memory=False)
 
-#1. Número de casos de Contagiados en el País.
-index = df.index
-ans = len(index)
-print(ans)
+df.rename(columns={'ID de caso': 'ID'}, inplace=True)
 
-#2. Número de Municipios Afectados
-ans = df.groupby(['Nombre municipio']).size().count()
-print(ans)
+df['Ubicación del caso'].replace('CASA', 'Casa', inplace=True)
+df['Recuperado'].replace('fallecido', 'Fallecido', inplace=True)
+df['Tipo de contagio'].replace('relacionado', 'Relacionado', inplace=True)
+df['Tipo de contagio'].replace('RELACIONADO', 'Relacionado', inplace=True)
+df['Tipo de contagio'].replace('EN ESTUDIO', 'En estudio', inplace=True)
+df['Tipo de contagio'].replace('En Estudio', 'En estudio', inplace=True)
+df['Sexo'].replace('m', 'M', inplace=True)
+df['Sexo'].replace('f', 'F', inplace=True)
 
-#3. Liste los municipios afectados (sin repetirlos)
-ans = df.groupby(['Nombre municipio']).size()
-print(ans)
+print('\n-------EJERCICIO1-------')
+print('numero de casos de contagio: ' + str(len(df)))
 
-#4. Número de personas que se encuentran en atención en casa
+print('\n-------EJERCICIO2-------')
+municipios = df['Nombre municipio'].unique()
+print('numero de municipios afectados: ' + str(len(municipios)))
 
-#5. Número de personas que se encuentran recuperados
+print('\n-------EJERCICIO3-------')
+print('municipios afectados: \n' + str(df['Nombre municipio'].unique()))
 
-#6. Número de personas que ha fallecido
-
-#7. Ordenar de Mayor a menor por tipo de caso (Importado, en estudio,
-#Relacionado)
-
-#8. Número de departamentos afectados
-
-#9. Liste los departamentos afectados(sin repetirlos)
-
-#10. Ordene de mayor a menor por tipo de atención
-
-#11. Liste de mayor a menor los 10 departamentos con mas casos de
-#contagiados
-
-#12. Liste de mayor a menor los 10 departamentos con mas casos de
-#fallecidos
-
-#13. Liste de mayor a menor los 10 departamentos con mas casos de
-#recuperados
-
-#14. Liste de mayor a menor los 10 municipios con mas casos de
-#contagiados
-
-#15. Liste de mayor a menor los 10 municipios con mas casos de
-#fallecidos
-
-#16. Liste de mayor a menor los 10 municipios con mas casos de
-#recuperados
-
-#17. Liste agrupado por departamento y en orden de Mayor a menor las
-#ciudades con mas casos de contagiados
-
-#18. Número de Mujeres y hombres contagiados por ciudad por
-#departamento
-
-#19. Liste el promedio de edad de contagiados por hombre y mujeres por
-#ciudad por departamento
-
-#20. Liste de mayor a menor el número de contagiados por país de
-#procedencia
-
-#21. Liste de mayor a menor las fechas donde se presentaron mas
-#contagios
-
-#22. Diga cual es la tasa de mortalidad y recuperación que tiene toda 
-#Colombmia
-
-#23. Liste la tasa de mortalidad y recuperación que tiene cada
-#departamento
-
-#24. Liste la tasa de mortalidad y recuperación que tiene cada ciudad
-
-#25. Liste por cada ciudad la cantidad de personas por atención
-
-#26. Liste el promedio de edad por sexo por cada ciudad de contagiados
-
-#27. Grafique las curvas de contagio, muerte y recuperación de toda
-#Colombia acumulados
-
-#28. Grafique las curvas de contagio, muerte y recuperación de los 10
-#departamentos con mas casos de contagiados acumulados
-
-#29. Grafique las curvas de contagio, muerte y recuperación de las 10
-#ciudades con mas casos de contagiados acumulados
-
-#30. Liste de mayor a menor la cantidad de fallecidos por edad en toda
-#Colombia.
-
-#31. Liste el porcentaje de personas por atención de toda Colombia
-
-#32. Haga un gráfico de barras por atención de toda Colombia
-
-#33. Haga un gráfico de barras por Sexo de toda Colombia
-
-#34. Haga un gráfico de barras por tipo de toda Colombia
-
-#35. Haga un gráfico de barras del número de contagiados, recuperados y
-#fallecidos por fecha de toda Colombia
+print('\n-------EJERCICIO4-------')
+atencion_casa = df[df['Ubicación del caso'] == 'Casa']
+print('numero de personas en casa: ' + str(len(atencion_casa)))
